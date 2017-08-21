4671f1b49a943f960e4dfb816b6d6a2da81cf3b0
Syk Houdeib <syk.houdeib@gmail.com>
2017-08-21 11:57:17 +0200

    Slide 2 structure
    Worked on the structure of slide 2.
    Added jQuery, fontawesome, and fonts.
    Applied some general styles
    added Jquery function to show/hide paragraphs.
    Created slide 2 grid and added icons and info.

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+$(document).ready();
+
+$(function() {
+    $('.icon1-s2').click(function() {
+        $('.info1-s2').toggle();
+    });                        
+});
+$(function() {
+    $('.icon2-s2').click(function() {
+        $('.info2-s2').toggle();
+    });                        
+});
+$(function() {
+    $('.icon3-s2').click(function() {
+        $('.info3-s2').toggle();
+    });                        
+});
+$(function() {
+    $('.icon4-s2').click(function() {
+        $('.info4-s2').toggle();
+    });                        
+});
