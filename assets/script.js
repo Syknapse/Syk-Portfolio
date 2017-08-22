fa19421a18072b482d7e1ceea13ed0af37174026
Syk Houdeib <syk.houdeib@gmail.com>
2017-08-22 12:03:18 +0200

    minor work
    changed nav buttons to links
    set up section index and links
    tried out few idea for scroll animation and info show

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,8 @@ $(function() {
 $(function() {
     $('.icon2-s2').click(function() {
         $('.info2-s2').toggle();
-    });                        
+        $('.info2-s2').addClass('animated fadeInUp');
+    });                      
 });
 $(function() {
     $('.icon3-s2').click(function() {
